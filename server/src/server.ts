75914691248b89b39d4743dcee5527b3de6a8104
bofmar <bofmar@outlook.com>
2023-07-26 18:39:21 +0300

    Create connection to mongoDB

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,24 @@
+/* ----------IMPORT LIBRARIES---------- */
 import express, {Request, Response} from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+/* ----------IMPORT MIDDLEWARE---------- */
 import morgan from "morgan";
+
+dotenv.config();
+
+// Constants
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
 
+mongoose.connect(MONGO_URI as string).then(_result => {
+	app.listen(PORT, () => console.log(`Listening for requests on port ${PORT}`));
+});
+
 app.use(morgan('combined'));
 
-app.listen(5000, () => console.log('Listening for requests on port 5000'));
 
 app.get('/', (_req: Request, res: Response) => {
 	res.json({msg: 'Hi'});
