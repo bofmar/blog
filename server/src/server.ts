6e83d04483dc86260b6fb3944b263137c4b7b3d3
bofmar <bofmar@outlook.com>
2023-07-26 18:22:32 +0300

    Add morgan middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
 import express, {Request, Response} from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan('combined'));
 
 app.listen(5000, () => console.log('Listening for requests on port 5000'));
 
