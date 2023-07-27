334e86d87e04b6ee53ed269e806266c777046dd0
bofmar <bofmar@outlook.com>
2023-07-27 13:22:56 +0300

    Add validation for the user

--- server/src/controllers/userControllers.ts
+++ server/src/controllers/userControllers.ts
@@ -1,7 +1,25 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import z from "zod";
+import { TUser } from "../models/user.js";
+
+const ZUser = z.object({
+	username: z.string().trim().min(2, {message: 'Usernames must be at least 2 characters long.'}),
+	password: z.string().trim().min(6, {message: 'Passwords must be at least 6 characters long.'}),
+});
+
+function validateUser(user: TUser) {
+	return ZUser.safeParse(user);
+}
 
 export const createUser = async (req: Request, res: Response) => {
 	try {
+		const validationResult = validateUser(req.body);
+		if(!validationResult.success) {
+			// TO DO trim the error message
+			res.status(400).json({success: false, errors: validationResult.error, data: req.body});
+			return;
+		}
+
 		res.json({msg: req.body});
 	} catch {
 		console.log('error');
