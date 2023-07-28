4c33705c6991d091f10318f2a753a9d1a455b0f4
bofmar <bofmar@outlook.com>
2023-07-28 11:43:07 +0300

    Handle creating new users in the db

--- server/src/controllers/userControllers.ts
+++ server/src/controllers/userControllers.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import z from "zod";
-import { TUser } from "../models/user.js";
+import User, { TUser } from "../models/user.js";
+import bcrypt from "bcryptjs";
 
 const ZUser = z.object({
-	username: z.string().trim().min(2, {message: 'Usernames must be at least 2 characters long.'}),
-	password: z.string().trim().min(6, {message: 'Passwords must be at least 6 characters long.'}),
+	username: z.string({required_error: "Please provide a username."}).trim().min(2, {message: 'Usernames must be at least 2 characters long.'}),
+	password: z.string({required_error: "Pleases provide a password."}).trim().min(6, {message: 'Passwords must be at least 6 characters long.'}),
 });
 
 function validateUser(user: TUser) {
@@ -15,13 +16,32 @@ export const createUser = async (req: Request, res: Response) => {
 	try {
 		const validationResult = validateUser(req.body);
 		if(!validationResult.success) {
-			// TO DO trim the error message
-			res.status(400).json({success: false, errors: validationResult.error, data: req.body});
+			res.status(400).json({success: false, errors: validationResult.error.flatten(), data: req.body});
 			return;
 		}
 
-		res.json({msg: req.body});
-	} catch {
-		console.log('error');
+		// Check if the username is in use
+		const userExists = await User.findOne({username: req.body.username}).exec();
+		if(userExists) {
+			res.status(400).json({success: false,
+								 errors: 
+									 {fieldErrors: { username: [ `Someone is already using ${req.body.username} as their username.` ] }},
+								 data: req.body});
+			return;
+		}
+
+		// create hash and salt
+		const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+
+		const newUser = new User({
+			username: req.body.username,
+			password: hashedPassword,
+			authLevel: 'USER'
+		});
+
+		await newUser.save();
+		res.json({success: true, errors: null, data: newUser});
+	} catch (error){
+		console.log(error);
 	}
 };
