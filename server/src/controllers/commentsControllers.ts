c24876b60e1c98bb849c6cdb0bac5875936479fc
bofmar <bofmar@outlook.com>
2023-08-01 10:38:19 +0300

    Handle getting comment by id

--- server/src/controllers/commentsControllers.ts
+++ server/src/controllers/commentsControllers.ts
@@ -3,11 +3,23 @@ import Comment from "../models/comment.js";
 
 export const allComments = async (_req: Request, res: Response, next: NextFunction) => {
 	try {
-		const comments = await Comment.find({}).exec();
-		if(!comments || comments.length === 0) {
+		const comments = await Comment.find({}).populate('createdBy').exec();
+		if (!comments || comments.length === 0) {
 			return res.status(400).json({success: false, errors: null, data: null});
 		}
 		res.json({success: true, errors: null, data: comments});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export const oneComment = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const comment = await Comment.findById(req.params.commentId).populate('createdBy').exec();
+		if (!comment) {
+			return res.status(400).json({success: false, errors: null, data: null});
+		}
+		res.json({success: true, errors: null, data: comment});
 	} catch (error) {
 		next(error);
 	}
--- server/src/routes/comments.ts
+++ server/src/routes/comments.ts
@@ -3,9 +3,7 @@ import express from 'express';
 import * as commentsControls from "../controllers/commentsControllers.js"
 
 // TODO:
-// Get all comments
 // Get all by user
-// Get one by id
 // Post new comment
 // Delete comment
 // Update comment
@@ -13,6 +11,8 @@ import * as commentsControls from "../controllers/commentsControllers.js"
 const route = express.Router();
 
 route.get('/', commentsControls.allComments);
+
+route.get('/:commentId', commentsControls.oneComment);
 
 
 export default route;
