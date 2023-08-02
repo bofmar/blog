c22e49006811a9bf0494317979f99f1583527504
bofmar <bofmar@outlook.com>
2023-08-02 12:15:02 +0300

    Add basic routing

--- panel_client/src/App.tsx
+++ panel_client/src/App.tsx
@@ -1,12 +1,24 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css'
 import LogIn from './components/LogIn';
-//import NewPost from './components/NewPost';
+import NewPost from './components/NewPost';
 import 'react-toastify/dist/ReactToastify.css';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <LogIn />
+	},
+	{
+		path: '/control-panel',
+		element: <NewPost />
+	}
+]);
 
 function App() {
 	return (
 	<div className='bg-gradient-to-br from-zinc-950 to-zinc-800 h-screen'>
-		<LogIn />
+		<RouterProvider router={router} />
 	</div>
 	);
 }
--- panel_client/src/main.tsx
+++ panel_client/src/main.tsx
@@ -2,22 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import NewPost from './components/NewPost.tsx'
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />
-	},
-	{
-		path: '/control-panel',
-		element: <NewPost />
-	}
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-	<RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
