21bf65be2cd98ea4203e7ba8d446a351a37822de
bofmar <bofmar@outlook.com>
2023-08-02 18:45:06 +0300

    Create the posts model

--- server/src/models/blogPost.ts
+++ server/src/models/blogPost.ts
@@ -1,16 +1,22 @@
-// TODO:
-// This is just a stub. We need to create a propper schema
-// when we have the info of what our posts will look like.
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
+import Comment from "./comment.js";
 
 const Schema = mongoose.Schema;
 
 export type TPost = {
 	title: string;
+	summary: string;
+	body: string;
+	likes: number;
+	comments: Array<Types.ObjectId>;
 }
 
 const PostSchema = new Schema<TPost>({
-	title: { type: String, required: true }
+	title: { type: String, required: true, minlength: 1 },
+	summary: { type: String, required: true, minlength: 20, maxlength: 250 },
+	body: { type: String, required: true, minlength: 1},
+	likes: { type: Number, required: true, default: 0 },
+	comments: [{ type: Schema.Types.ObjectId, ref: Comment.modelName, required: true }]
 }, { timestamps: true });
 
 const BlogPost = mongoose.model<TPost>('User', PostSchema);
