9449abfcd35b9426d506d4cc6ba78b51b85156e4
bofmar <bofmar@outlook.com>
2023-08-04 16:25:48 +0300

    Style the unpablished route

--- panel_client/src/components/Unpablished.tsx
+++ panel_client/src/components/Unpablished.tsx
@@ -1,20 +1,20 @@
-import { NavLink } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import { IPost } from "../types";
 import URI from "../uri";
+import PostCard from "./PostCard";
 
 export default function Unpublished() {
 	const Uri = new URI;
 	const {data: posts, error, loading} = useFetch<Array<IPost>>(Uri.posts);
 
 	return (
-	<div>
+	<div className="col-span-10 flex flex-col justify-center content-center my-32">
 		{error && !posts && <p className="text-white">ERROR</p>}
 		{loading && <p className="text-white">loading...</p>}
-		{posts && <ul className="text-white">
+		{posts && <ul className="text-white flex flex-col gap-20 justify-center content-center items-center">
 					{posts.map(post => post.status === 'UNPUBLISHED' ?
 						<li key={post._id}>
-							<NavLink to={`/posts/${post._id}`}>{post.title}</NavLink>
+							<PostCard post={post} />
 						</li>  
 					:  null)}
 					</ul>}
