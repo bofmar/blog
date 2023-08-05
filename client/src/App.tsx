3a7389d072151a4d3d4c1c007205c0859c68dbc7
bofmar <bofmar@outlook.com>
2023-08-05 17:35:20 +0300

    Incoroporate react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css'
+import Home from './components/Home';
 
 function App() {
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <Home />
+		},
+	]);
 
 	return (
-			<div className='bg-gradient-to-br from-zinc-950 to-zinc-800 min-h-screen'>
-			Hello
-			</div>
+		<div className='bg-gradient-to-br from-zinc-950 to-zinc-800 min-h-screen'>
+			<RouterProvider router={router} />
+		</div>
 	);
 }
 
