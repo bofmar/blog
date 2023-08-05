d5d60110ee03af902e061caa555013c49072fdfd
bofmar <bofmar@outlook.com>
2023-08-05 13:30:48 +0300

    Finish the AllUsers route

--- panel_client/src/components/AllUsers.tsx
+++ panel_client/src/components/AllUsers.tsx
@@ -1,21 +1,21 @@
 import { IUser } from "../types";
 import useFetch from "../hooks/useFetch";
 import URI from "../uri";
-import { NavLink } from "react-router-dom";
+import UserCard from "./UserCard";
 
 export default function AllUsers() {
 	const Uri = new URI;
 	const {data: users, error, loading} = useFetch<Array<IUser>>(Uri.users);
 
 	return (
-	<div>
+	<div className="col-span-10 flex flex-col justify-center content-center items-center my-32">
 		{error && !users && <p className="text-white">ERROR</p>}
 		{loading && <p className="text-white">loading...</p>}
-		{users && <ul className="text-white">
-						{users.map(user => <li key={user._id}>
-							<NavLink to={`/posts/${user._id}`}>{user.username}</NavLink>
+		{users && <ol className="text-white">
+						{users.map((user, i) => <li key={user._id}>
+							<UserCard user={user} index={i} />
 						</li>)}
-					</ul>}
+					</ol>}
 	</div>
 	);
 }
---
+++ panel_client/src/components/UserCard.tsx
@@ -0,0 +1,25 @@
+import { IUser } from "../types";
+
+interface IUserCardProps {
+	user: IUser;
+	index: number
+}
+
+function padLeft(num: number) {
+	let str = num.toString();
+	for (let i = str.length; i < 3; i++) {
+		str = ' ' + str;
+	}
+	console.log(str);
+	return str;
+}
+
+export default function UserCard({user, index}: IUserCardProps) {
+	return (
+		<div className="font-sans w-[600px] flex justify-center items-center">
+			<span className="text-stone-300 text-lg">{`${padLeft(index + 1)}. `}</span>
+			<h1 className="text-stone-300 text-lg">{user.username}</h1>
+			<span className={`pr-10 font-bold ${user.authLevel === 'ADMIN' ? 'text-amber-300' : 'text-cyan-500'}`}>{` - ${user.authLevel}`}</span>
+		</div>
+	);
+}
